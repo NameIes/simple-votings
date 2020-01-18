6da3618dd2574a36fa6a12ed91b5a178b5deeb75
Andrew Piryazev <piryazev777@gmail.com>
2020-01-18 23:11:08 +0300

    reset is ready

--- simple_votings/simple_votings_app/urls.py
+++ simple_votings/simple_votings_app/urls.py
@@ -29,7 +29,9 @@ urlpatterns = [
     path('password_reset/done/',
          au_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
          name='password_reset_done'),
-
+    path('password_reset_complete/',
+         au_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 
